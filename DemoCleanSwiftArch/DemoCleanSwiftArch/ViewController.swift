ff2a08de0507815a26f096b41b7cd7000d391c4d
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-10-25 11:23:55 -0700

    initial commit
    - Project creation

---
+++ DemoCleanSwiftArch/DemoCleanSwiftArch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCleanSwiftArch
+//
+//  Created by Hiram Castro Maldonado on 25/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
